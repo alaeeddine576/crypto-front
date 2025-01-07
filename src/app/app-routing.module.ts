91a51400df0ef1ff035dece1dc3b3d41bde6dc19
alaeeddine <alaeeddine576@gmail.com>
2025-01-07 18:04:24 +0100

    add track

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TrackComponent } from './views/track/track.component';
 
 const routes: Routes = [
+  {
+    path: 'track',
+    component: TrackComponent
+  },
   {
     path: '',
     loadChildren: () => import('./views/views.module').then(m => m.ViewsModule)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HotToastModule } from '@ngneat/hot-toast';
 import { ViewsModule } from './views/views.module';
+import { TrackComponent } from './views/track/track.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrackComponent
   ],
   imports: [
     BrowserModule,
